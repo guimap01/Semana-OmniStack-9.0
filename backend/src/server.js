dbd664f8a897d011c70309f7da71241cced064fd
Pedro Guimaraes <pedromonteirogui@gmail.com>
2020-05-01 16:32:16 -0300

    Frontend with login page finished

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const routes = require("./routes");
 
 const app = express();
 
 mongoose.connect(
-  "mongodb+srv://guimap01:cbm2exfw@omnistack-uc8oa.mongodb.net/omnistack9?retryWrites=true&w=majority", {
+  "mongodb+srv://guimap01:cbm2exfw@omnistack-uc8oa.mongodb.net/omnistack9?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  });
+  }
+);
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
