1b417b1df3989c6ac56bb01a17711bf7634b1e60
Pedro Guimaraes <pedromonteirogui@gmail.com>
2020-04-30 23:08:43 -0300

    Finished Backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
+const SessionController = require("./controllers/SessionController");
+const DashboardController = require("./controllers/DashboardController");
+const BookingController = require("./controllers/BookingController");
+
+
+
+const SpotController = require("./controllers/SpotController");
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
-routes.post("/users", (req, res) => {
-  return res.json(req.body);
-});
+routes.post("/sessions", SessionController.store);
+
+routes.get("/spots", SpotController.index);
+routes.post("/spots", upload.single("thumbnail"), SpotController.store);
+
+routes.get("/dashboard", DashboardController.show);
+
+routes.post("/spots/:spot_id/booking", BookingController.store);
+
 
 module.exports = routes;
